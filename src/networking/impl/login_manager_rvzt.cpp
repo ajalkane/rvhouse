0be382ffbd7795547a8e46f89f3fc29b7e73cfa4
ajalkane <ajalkane@9636466f-091a-0410-9f03-f2f3d0fdbb11>
2009-05-01 19:06:39 +0000

    Crashing problem when trying to login when server down fixed

--- src/networking/impl/login_manager_rvzt.cpp
+++ src/networking/impl/login_manager_rvzt.cpp
@@ -14,7 +14,7 @@ namespace networking {
 
 #define LOGIN_MESSAGE(msg) (msg - message::login + _msg_base)
 
-login_manager_rvzt::login_manager_rvzt(ACE_Reactor *r) 
+login_manager_rvzt::login_manager_rvzt(ACE_Reactor *r)
   : login_manager(r),
     _http_fetcher(new http::fetcher)
 {
@@ -24,16 +24,16 @@ login_manager_rvzt::login_manager_rvzt(ACE_Reactor *r)
     _base_register_url = net_conf()->get<std::string>(
         "auth", "register", "http://revolt.speedweek.net/main/user_register.php"
     );
-    
+
     // Ensure only revolt.speedweek.net or www.revoltrace.com used
     http::url validate_url = http::url(_base_validate_url);
     http::url register_url = http::url(_base_register_url);
-    
+
     if (!_check_auth_url(validate_url))
         throw exception(0, "Invalid validation URL");
     if (!_check_auth_url(register_url))
         throw exception(0, "Invalid register URL");
-        
+
 }
 
 bool login_manager_rvzt::_check_auth_url(const http::url &u)
@@ -48,9 +48,9 @@ login_manager_rvzt::~login_manager_rvzt() {
     delete _http_fetcher;
 }
 
-void 
+void
 login_manager_rvzt::_login_operation(
-  const std::string &req_url, 
+  const std::string &req_url,
   const std::string &user,
   int _msg_base
 ) {
@@ -61,18 +61,20 @@ login_manager_rvzt::_login_operation(
     } catch (std::exception &e) {
         ACE_ERROR((LM_ERROR, "login_manager_rvzt::_login_operation exception '%s'\n",
                   e.what()));
-        
+
         gui_messenger()->send_msg(
           new message(LOGIN_MESSAGE(message::login_err)));
-        delete h;
+        // No need for deleting the handler, seems like ACE calls
+        // handle_close() if could not connect
+        // delete h;
     }
     fh_guard.release();
 }
 
-void 
+void
 login_manager_rvzt::login(const message *msg) {
     const message_login *m = dynamic_ptr_cast<const message_login>(msg);
-    
+
     std::string req_url = _base_validate_url;
     req_url += "?user=" + m->user();
     req_url += "&pass=" + m->pass();
@@ -80,35 +82,35 @@ login_manager_rvzt::login(const message *msg) {
     _login_operation(req_url, m->user(), message::login);
 }
 
-void 
+void
 login_manager_rvzt::register_user(const message *msg) {
     const message_register *m = dynamic_ptr_cast<const message_register>(msg);
-    
+
     std::string req_url = _base_register_url;
     req_url += "?user=" + m->user();
     req_url += "&pass=" + m->pass();
     req_url += "&mail=" + m->mail();
 
-    _login_operation(req_url, m->user(), message::reg); 
+    _login_operation(req_url, m->user(), message::reg);
 }
 
-// http::handler interface      
+// http::handler interface
 int
-login_manager_rvzt::_fetch_handler::handle_response(const http::response &resp) 
+login_manager_rvzt::_fetch_handler::handle_response(const http::response &resp)
 {
     ACE_DEBUG((LM_DEBUG, "login_manager_rvzt: got response: %s\n",
               resp.content()));
     const char *str = resp.content();
     std::vector<std::string> res(2);
-    
+
     if (regexp::match("^([^:]{3,20}):(.*)", str, res.begin())) {
     // if (regexp::match("^([a-zA-Z0-9_]{3,20}):(.*)", str, res.begin())) {
         const std::string &user = res[0];
         const std::string &stat = res[1];
-        
+
         bool validated = false;
-        
-        ACE_DEBUG((LM_DEBUG, "login_manager: got user '%s' status '%s'\n", 
+
+        ACE_DEBUG((LM_DEBUG, "login_manager: got user '%s' status '%s'\n",
                    user.c_str(), stat.c_str()));
         if (strcasecmp(user.c_str(), _user.c_str())) {
             ACE_DEBUG((LM_DEBUG, "login_manager: server returned differing " \
@@ -119,7 +121,7 @@ login_manager_rvzt::_fetch_handler::handle_response(const http::response &resp)
         } else {
             validated = true;
         }
-        
+
         if (validated) {
             gui_messenger()->send_msg(
               new message_login(LOGIN_MESSAGE(message::login_done),
@@ -128,14 +130,14 @@ login_manager_rvzt::_fetch_handler::handle_response(const http::response &resp)
             gui_messenger()->send_msg(
               new message(LOGIN_MESSAGE(message::login_fail)));
         }
-            
+
     } else {
-        ACE_DEBUG((LM_DEBUG, "login_manager_rvzt: could not parse response: %s\n", 
+        ACE_DEBUG((LM_DEBUG, "login_manager_rvzt: could not parse response: %s\n",
         resp.content()));
         gui_messenger()->send_msg(
           new message(LOGIN_MESSAGE(message::login_err)));
     }
-    
+
     return 0;
 }
 
@@ -144,7 +146,7 @@ login_manager_rvzt::_fetch_handler::handle_error(int reason, const char *details
     ACE_DEBUG((LM_DEBUG, "login_manager_rvzt: error: %s\n", details));
     gui_messenger()->send_msg(
       new message(LOGIN_MESSAGE(message::login_err)));
-    return 0;   
+    return 0;
 }
 
 int
@@ -152,8 +154,8 @@ login_manager_rvzt::_fetch_handler::handle_close() {
     ACE_DEBUG((LM_DEBUG, "login_manager_rvzt: destroying handler\n"));
     delete this;
     ACE_DEBUG((LM_DEBUG, "login_manager_rvzt: destroying handler, done\n"));
-    return 0;   
+    return 0;
 }
-    
+
 
 } // ns networking
