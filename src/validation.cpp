e6f1222fe50897563152ec24a79c9a54bf24ae9f
ajalkane <ajalkane@9636466f-091a-0410-9f03-f2f3d0fdbb11>
2011-07-26 22:02:39 +0000

    New RVH registration limits

--- src/validation.cpp
+++ src/validation.cpp
@@ -2,22 +2,22 @@
 #include "regexp.h"
 
 bool validate_user_id (const std::string &u) {
-    if (u.size() < 3)  return false;
-    if (u.size() > 20) return false;
+    if (u.size() < 4) return false;
+    //Hmm really no upper limit?
+    //if (u.size() > 20) return false;
     
     return regexp::match("^[a-zA-Z0-9_\\-\\[\\]=]+$", u);
-    //return regexp::match("^[a-zA-Z0-9\\[\\]_-=]+$", u);
-    //return regexp::match("^[a-zA-Z0-9_]+$", u);
 }
 
 bool validate_password(const std::string &p) {
-    if (p.size() < 3)   return false;
-    if (p.size() > 255) return false;
+    if (p.size() < 6) return false;
+    //Hmm really no upper limit?
+    //if (p.size() > 255) return false;
     
-//    return regexp::match("^[\\w]+$", p);
     return regexp::match("^[a-zA-Z0-9_]+$", p);
 }
 
 bool validate_email   (const std::string &e) {
+    if (p.size() < 7) return false;
     return regexp::match(".+@.+\\..+", e);  
 }
