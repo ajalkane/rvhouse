3bafa09d30842f8ecd5b769ee6070d47444d987f
huki <gk7huki@gmail.com>
2015-10-29 13:26:38 +0530

    Incremented app version.

--- src/app_version.h
+++ src/app_version.h
@@ -1,8 +1,8 @@
 #ifndef _APP_VERSION_H_
 #define _APP_VERSION_H_
 
-#define APP_NAME    "RV House (No more Tea Edition)"
-#define APP_VERSION_ONLY "0.93.6"
+#define APP_NAME    "RV House (Dinosaur Edition)"
+#define APP_VERSION_ONLY "0.93.7"
 #define APP_VERSION APP_VERSION_ONLY
 
 // Needed for example by QSettings to organize where settings are stored
